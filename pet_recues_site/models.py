a99c31cb0fd387efe7089c89391ff7d111b0c880
Thanh Khiet Huynh <20650982@student.westernsydney.edu.au>
2023-10-10 13:23:15 +1100

    Create user class

--- pet_recues_site/models.py
+++ pet_recues_site/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User (models.Model):
+    user_name = models.CharField(max_length=255)
+    user_email = models.EmailField(unique=True, primary_key=True)
+    fisrt_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    suburb = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=10)
+    DOB = models.DateField()
+    joined_date = models.DateField()
+
+
+
+
