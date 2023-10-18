280dfc9b083d2c9b7461c51f049f114bf4e119c2
Thanh Khiet Huynh <20650982@student.westernsydney.edu.au>
2023-10-18 22:31:47 +1100

    adding registration form and sign up function in views.py

--- SEF_Assignment_COMP2021/rescue_pet_website/rescues_site/urls.py
+++ SEF_Assignment_COMP2021/rescue_pet_website/rescues_site/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('service/', views.service, name='service'),
     path('admin_user/', views.admin_user, name='admin_user'),
     path('pet_list/', views.pet_list, name='pet_list'),
+    path('registration_form/', views.registration_form, name='registration_form'),
+    path('registration_form/sign_up', views.sign_up, name='sign_up'),
 ]
--- SEF_Assignment_COMP2021/rescue_pet_website/rescues_site/views.py
+++ SEF_Assignment_COMP2021/rescue_pet_website/rescues_site/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db import transaction
+from .models import CustomUser, User
 
 pets = [
     {
@@ -37,3 +39,47 @@ def pet_list(request):
 def admin_user(request):
     return render(request, 'rescues_site/admin_user.html')
 
+def registration_form(request):
+    return render(request, 'registration/sign_up.html')
+
+@transaction.atomic
+def sign_up(request):
+
+    if request.method == 'POST':
+        error = []
+
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        address = request.POST.get('address')
+        suburb = request.POST.get('suburb')
+        postcode = request.POST.get('postcode')
+        email = request.POST.get('email')
+        phone_number = request.POST.get('phone_number', None)
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.create_user(username = email, 
+                                             first_name = first_name, 
+                                             last_name = last_name, 
+                                             password = password)
+        
+        except Exception as e:
+            error.append(str(e))
+            print(f"User creation failed: {e}")
+            return render(request, 'registration/sign_up.html', {'error': error})
+        
+        new_user = CustomUser(
+            address = address,
+            suburb = suburb,
+            postcode = postcode,
+            phone_number = phone_number
+        )
+        
+        try:
+            new_user.save()
+        except Exception as e:
+            error.append(str(e))
+            print(f"User creation failed: {e}")
+            return render(request, 'registration/sign_up.html', {'error': error})
+        
+    return redirect('home_page')
